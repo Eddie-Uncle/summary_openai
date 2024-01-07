524fc028bcece3b1942dac45eb6033168ec651f1
Bernardo <bernardsp@gmail.com>
2024-01-07 12:37:37 -0300

    openai migrate

--- summary.py
+++ summary.py
@@ -1,24 +1,23 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def read_file(file):
     with open(file, 'r', encoding='utf-8') as file:
         return file.read()
     
 def summary(text):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Summarize the following text: {text}",
-        temperature=0.7,
-        max_tokens=2048,
-        n=1,
-        stop=None
-    )
+    response = client.completions.create(engine="text-davinci-003",
+    prompt=f"Summarize the following text: {text}",
+    temperature=0.7,
+    max_tokens=2048,
+    n=1,
+    stop=None)
     return response['choices'][0]['text'].strip()
 
 file = 'article.txt'
